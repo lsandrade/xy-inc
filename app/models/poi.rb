3da9411d37727c786fbc295d7b7efc739f515e6d
Luan Andrade <luansandrade92@hotmail.com>
2016-07-24 23:02:57 -0300

    calculo da distancia entre 2 pontos

--- app/models/poi.rb
+++ app/models/poi.rb
@@ -1,2 +1,6 @@
 class Poi < ActiveRecord::Base
+
+  def distance(a,b)
+    Math.sqrt((a[0]-b[0])**2+(a[1]-b[1])**2)
+  end
 end
--- test/models/poi_test.rb
+++ test/models/poi_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class PoiTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     p = Poi.new
+     distance = p.distance([4,5],[1,1])
+     assert_equal(5,distance)
+     #puts distance
+   end
 end
